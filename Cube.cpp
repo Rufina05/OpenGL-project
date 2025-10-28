077fe6a4f5c6ad5176973c55c8e6c01f9b6cdde5
Rufina05 <vip.rummi@gmail.com>
2025-10-28 08:50:59 +0200

    testare 1

--- Cube.cpp
+++ Cube.cpp
@@ -2,24 +2,33 @@
 
 void drawCube(float size) {
     float s = size / 2.0f;
+    // ¬ершины куба по координатам
     float verts[8][3] = {
-        {-s, -s, -s}, {s, -s, -s}, {s, s, -s}, {-s, s, -s},
-        {-s, -s, s},  {s, -s, s},  {s, s, s},  {-s, s, s}
+        {-s,  s, -s}, // [0] 
+        { s,  s, -s}, // [1] 
+        { s, -s, -s}, // [2] 
+        {-s, -s, -s}, // [3] 
+        {-s,  s,  s}, // [4] 
+        { s,  s,  s}, // [5] 
+        { s, -s,  s}, // [6] 
+        {-s, -s,  s}  // [7] 
     };
-
+    // √рани куба
     int faces[6][4] = {
-        {4,5,6,7}, // передн€€
-        {0,1,2,3}, // задн€€
-        {0,4,7,3}, // лева€
-        {1,5,6,2}, // права€
-        {3,7,6,2}, // верхн€€
-        {0,4,5,1}  // нижн€€
+        {4, 5, 6, 7}, 
+        {1, 0, 3, 2}, 
+        {0, 4, 7, 3}, 
+        {5, 1, 2, 6}, 
+        {0, 1, 5, 4}, 
+        {3, 7, 6, 2}  
     };
 
     glBegin(GL_QUADS);
     glColor3f(0.55f, 0.27f, 0.07f); // коричневый
     for (int i = 0; i < 6; i++)
         for (int j = 0; j < 4; j++)
-            glVertex3f(verts[faces[i][j]][0], verts[faces[i][j]][1], verts[faces[i][j]][2]);
+            glVertex3f(verts[faces[i][j]][0], 
+                       verts[faces[i][j]][1],
+                       verts[faces[i][j]][2]);
     glEnd();
 }
--- Sphere.cpp
+++ Sphere.cpp
@@ -1,28 +1,39 @@
-#include <GLFW/glfw3.h>
+﻿#include <GLFW/glfw3.h>
 #include <cmath>
 
-const int slices = 40;
-const int stacks = 40;
+const int sectorCount = 40;
+const int stackCount = 40;
 const float PI = 3.14159265358979323846f;
 
 void drawSphere(float radius) {
-    glColor3f(1.0f, 0.0f, 0.0f); // ������� ���
+    glColor3f(1.0f, 0.0f, 0.0f); // красный шар
 
-    for (int i = 0; i < stacks; i++) {
-        float lat0 = PI * (-0.5f + (float)i / stacks);
-        float lat1 = PI * (-0.5f + (float)(i + 1) / stacks);
-        float z0 = radius * sin(lat0), z1 = radius * sin(lat1);
-        float r0 = radius * cos(lat0), r1 = radius * cos(lat1);
+    for (int stackStep = 0; stackStep < stackCount; stackStep++) {
+        // Вычисляем углы φ для текущего и следующего стека
+        float phi0 = PI / 2.0f - PI * (float)stackStep / stackCount;
+        float phi1 = PI / 2.0f - PI * (float)(stackStep + 1) / stackCount;
 
-        glBegin(GL_QUAD_STRIP);  // ������ ������ ����� ������
-        for (int j = 0; j <= slices; j++) {
-            float lng = 2 * PI * j / slices;
-            float x0 = cos(lng) * r0, y0 = sin(lng) * r0;
-            float x1 = cos(lng) * r1, y1 = sin(lng) * r1;
+        // z 
+        float z0 = radius * sin(phi0);
+        float z1 = radius * sin(phi1);
+
+        glBegin(GL_QUAD_STRIP);
+
+        for (int sectorStep = 0; sectorStep <= sectorCount; sectorStep++) {
+            // Вычисляем угол θ для текущего сектора
+            float theta = 2.0f * PI * (float)sectorStep / sectorCount;
+
+            // x
+            float x0 = (radius * cos(phi0)) * cos(theta);
+            float x1 = (radius * cos(phi1)) * cos(theta);
+            // y
+            float y0 = (radius * cos(phi0)) * sin(theta);
+            float y1 = (radius * cos(phi1)) * sin(theta);
 
             glVertex3f(x0, y0, z0);
             glVertex3f(x1, y1, z1);
         }
+
         glEnd();
     }
 }
